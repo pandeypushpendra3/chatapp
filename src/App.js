3700e62ef7129405f5b372106a0695d07c0513be
pandeypushpendra3 <pandeypushpendra3@gmail.com>
2022-02-15 21:57:59 +0530

    right container part done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,87 @@
  import './App.css';
 import {React,useState,useEffect} from "react";
-import {multiSelect} from "multiselect-react-dropdown"
+import Multiselect from "multiselect-react-dropdown"
 
 function App() {
 const initStat ={username:"",email:"", password:"" ,primary_products:"",secondary_product_service_offered:"",business_area:"",establishment_area:"",customers:"",suppliers:"",age_of_establishment:""};
+const initOptionStat=()=>{
+  const value="";
+  return value;
+}
+const[value,setValue] = useState(initOptionStat);
+const[offered_to,setOffered_to] = useState(initOptionStat);
+const [skill_training,setSkill_training] = useState((initOptionStat));
+const [business_locality,setBusiness_locality] = useState(initOptionStat);
+const [infra_ownership,setInfra_ownership] = useState(initOptionStat);
+
+const [market_research,setMarket_research] = useState(initOptionStat);
+
+const [primary_market,setPrimary_market] = useState(initOptionStat);
+const [seasonality,setSeasonality] = useState(initOptionStat);
+const [competition,setCompetition] = useState(initOptionStat);
+const optionHandleChange=(e)=>{
+setValue(e.target.value);
+console.log(value);
+}
+
+const optionHandleChange1=(e)=>{
+  setOffered_to(e.target.value);
+  console.log(offered_to);
+}
+
+const optionHandleChange2=(e)=>{
+setSkill_training(e.target.value);
+console.log(skill_training);
+}
+const optionHandleChange3=(e)=>{
+setBusiness_locality(e.target.value);
+console.log(business_locality);
+}
+
+const optionHandleChange4=(e)=>{
+  setInfra_ownership(e.target.value)
+}
+const optionHandleChange5=(e)=>{
+  setMarket_research(e.target.value);
+
+}
+const optionHandleChange6=(e)=>{
+  setPrimary_market(e.target.value);
+
+}
 const [formvalue,setFormValue] = useState(initStat);
 const [formError,setFormError] = useState({});
 const [isSubmit,setSubmit] = useState(false);
+const data =["the entrepreneur's experience in this field",
+"innovative product/service",
+  "high profit margin",
+  "high growth potential",
+  "Superior customer support",
+  "quality and affordable products/service",
+  "high demand in the target market",
+  "abundant supply of raw materials",
+  "wide range of products/services",
+  "robust value-chain of the business",
+"frequency of customer visits"
+]
+// const location=[
+//   {data:""}]
+const market=["Distribution of marketing material(Pamphlets/flyers)","storefront/business branding","Seasonal discountsand offers","Cross-promotions","Digital Marketing","Word of mouth"]
+const scaleUp1=["Increasing the variety of product/service offering.","expanding the current offering to other geographies","Opening more oulets","growing the scale of operations","building value-chain integration","other allied revenue streams"]
+
+const locations=["Nearness to market","Good Footfall","Nearness to source of raw materials","Nearness to targeted customer demographics","Lesser competition around","Access to permit/license of operate","Nearness to suppliers","Affordability","No rental overheads","others such factors"]
+
+ const [location,setLocation]=useState(locations);
+ const [reasonLocation,setreasonLocation]=useState([]);
 
 
+const [scaleup_potential,setScaleup_potential] =useState(scaleUp1);
+const [scale_avenues,setScaleUp] = useState([]);
+const[marketstate,setMarketstate] = useState(market);
+// console.log(marketstate)
+const[market_search,setMarket] = useState([])
+const [state,setState]=useState(data);
+const [select,setSelect]= useState([]);
 const handleChange=(e)=>{
 //  console.log(e.target.value);
 
@@ -25,6 +98,7 @@ useEffect(()=>{
   console.log(formError);
   if(Object.keys(formError).length===0 && isSubmit){
 console.log(formvalue);
+;
   }
 },[formError])
 const validate=(formvalue)=>{
@@ -68,7 +142,7 @@ return (
   <p>{formError.username}</p>
   <div className="field">
     <label>Stage of business</label>
-<select id="stage" name="Business_stage">
+<select value={value} onChange={optionHandleChange} name="Business_stage">
 <option value="Start-up">Start-up</option>
 <option value="Scale-up">Scale-up</option>
 </select>
@@ -88,7 +162,8 @@ return (
 </div>
 <div className="field">
   <label>Offfered to</label>
-  <select id="offered" name="Offered to">
+  <select id="offered" name="offered_to" value={offered_to} onChange={optionHandleChange1}>
+  <option></option>
 <option value="End customers">End customers</option>
 <option value="Wholesalers">Wholesalers</option>
 <option value="Distributors">Distributors</option>
@@ -109,7 +184,7 @@ return (
 </div>
 <div className="field">
   <label>Skill training</label>
-   <select id="skill"  name="Skill" >
+   <select id="skill"  name="Skill"  value={skill_training} onChange={optionHandleChange2}>
    <option value="No formal skill training">No formal skill training</option>
    <option value="Has formal skill training and certificate">Has formal skill training and certificate</option>
 </select>
@@ -117,25 +192,12 @@ return (
 
 <div className="field">
   <label>Unique Selling Proposition</label>
-   <select id="skill"  name="usp" multiple>
-   <option value="the entrepreneur's experience in this field">the entrepreneur's experience in this field</option>
-   <option value=" innovative product/service"> innovative product/service</option>
-   <option value="high profit margin">high profit margin</option>
-   <option value="high growth potential">high growth potential</option>
-   <option value="Superior customer support">Superior customer support</option>
-   <option value="quality and affordable products/service">quality and affordable products/service</option>
-   <option value="high demand in the target market">high demand in the target market</option>
-
-   <option value="abundant supply of raw materials">abundant supply of raw materials</option>
-   <option value="wide range of products/services">wide range of products/services</option>
-   <option value="robust value-chain of the business">robust value-chain of the business</option>
-   <option value="frequency of customer visits">frequency of customer visits</option>
-</select>
-</div>
+   <Multiselect isObject={false} onRemove={(e)=> {console.log(e)}} onSelect={(e)=> {setSelect([...select,e]) }}  options={state}  />
+   </div>
  <h1>Location</h1>
  <div className="field">
     <label>Establishment type</label>
-   <select id="type">
+   <select id="type" value={business_locality} onChange={optionHandleChange3}>
      <option value="Factory">Factory</option>
      <option value="Mill">Mill</option>
      <option value="Workshop">Workshop</option>
@@ -159,7 +221,7 @@ return (
 </div>
 <div className="field">
   <label>Locality of business</label>
-    <select id="Locality" name="Locality of business">
+    <select id="Locality" name="Locality of business" value={business_locality} onChange={optionHandleChange3}>
 <option value="Urban">Urban</option>
 <option value="Rural">Rural</option>
 <option value="Semi-urban">Semi-urban</option>
@@ -168,7 +230,7 @@ return (
 </div>
 <div className="field">
   <label>Ownership of infrastructure</label>
-    <select id="ownership" name="Infra-Ownership">
+    <select id="ownership" name="Infra-Ownership" value={infra_ownership} onChange={optionHandleChange4}>
 <option value="Rented">Rented</option>
 <option value="Leased">Leased</option>
 <option value="Self-owned">Self-owned</option>
@@ -182,30 +244,19 @@ return (
    value={formvalue.establishment_area} placeholder="Establishment area" onChange={handleChange}/>
 </div>
 
+
+
 <div className="field">
   <label>Reason for selecting this location</label>
-
-  <select name="reason for location">
-    <option value="Nearness to market">Nearness to market</option>
-    <option value="Good Footfall">Good Footfall</option>
-    <option value="Nearness to source of raw materials">Nearness to source of raw materials</option>
-    <option value="Nearness to targeted customer demographics">Nearness to target customer demographics</option>
-
-<option value='Lesser competition around'>Lesser competition around</option>
-<option value='Access to permit/license of operate'>Access to permit/license of operate</option>
-<option value="Nearness to suppliers">Nearness to suppliers</option>
-<option value="Affordability">Affordability</option>
-<option value="No rental overheads">No rental overheads</option>
-<option value="others such factors">others such factors</option>
-</select>
-</div>
+  <Multiselect isObject={false} onRemove={(e)=> {console.log(e)}} onSelect={(e)=> {setreasonLocation([...reasonLocation,e]) }}  options={location} />
+ </div>
 
 
 <h1>Target Market</h1>
 
 <div className="field">
   <label>Market research</label>
-    <select name="market-research" id="market-research">
+    <select name="market-research" value={market_research} onChange={optionHandleChange5}>
       <option value="Not Conducted">Not Conducted</option>
       <option value="Market research has been conducted">Market research has been conducted</option>
       
@@ -213,7 +264,7 @@ return (
 </div>
 <div className="field">
   <label>Primary market</label>
-    <select name="primary-market" id="primary-market">
+    <select name="primary-market" value={primary_market} onChange={optionHandleChange6}>
       <option value="Local">Local</option>
       <option value="Regional">Regional</option>
       <option value="National">National</option>
@@ -227,7 +278,7 @@ return (
 </div>
 <div className="field">
   <label>Seasons of high sales</label>
-    <select name="seasonality" id="seasonality">
+    <select name="seasonality" value={seasonality} onChange={(e)=>setSeasonality(e.target.value)}>
       <option value="Consistent sales across all seasons">Consistent sales across all seasons</option>
       <option value="Higher sales in festive seasons">Higher sales in festive seasons</option>
       <option value="High sales in cropping/cultivation season">High sales in cropping/cultivation season</option>
@@ -237,7 +288,7 @@ return (
 
 <div className="field">
   <label>Competition</label>
-  <select id="competition" name="Competition" >
+  <select id="competition" name="Competition" value={competition} onChange={(e)=>setCompetition(e.target.value)}>
       <option value="No similar good/service provider in this locality">No similar good/service provider in this locality</option>
       <option value="Only a few similar goods/service providers in this locality">Only a few similar goods/service providers in this locality</option>
       <option value="Many similar good/service providers in this locality">Many similar good/service providers in this locality</option>
@@ -252,44 +303,47 @@ return (
 
 <div className="field">
   <label>Market avenues</label>
-    <select name="Market avenues" id="Market-avenues" multiple>
-      <option value="Distribution of marketing material(Pamphlets/flyers)">Distribution of marketing material(Psticks/flyers)</option>
-      <option value="storefront/business branding">storefront/business branding</option>
-      <option value="Seasonal discountsand offers">Seasonal discountsand offers</option>
-      <option value="Cross-promotions">Cross-promotion</option>      
-      <option value="Digital Marketing">Digital Marketing</option>
-      <option value="Word of mouth">Word of mouth</option>
-    </select>
+  <Multiselect isObject={false} onRemove={(e)=> {console.log(e)}} onSelect={(e)=> {setMarket([...market_search,e]) }}  options={marketstate}  />
 </div>
+
 <div className="field">
-  <label>Market avenues</label>
-    <select name="Market avenues" id="Market-avenues" multiple>
-      <option value="Increasing the variety of product/service offering."></option>
-      <option value="expanding the current offering to other geographies">expanding the current offering to other geographies</option>
-      <option value="Opening more oulets">Opening more outlets</option>
-      <option value="growing the scale of operations">growing the scale of operations</option>      
-      <option value="building value-chain integration">building value-chain integration</option>
-      <option value="other allied revenue streams">other allied revenue streams</option>
-    </select>
+  <label>Aveneus of scaling up in future</label>
+  <Multiselect isObject={false} onRemove={(e)=> {console.log(e)}} onSelect={(e)=> {setScaleUp([...scale_avenues,e]) }}  options={scaleup_potential} />
 </div>
 {/* <button className="fluid ui button blue" >Submit</button> */}
 </div>
      </form>
     </div>
     <div className="container-right">
-    <p> {formvalue.username}----- is looking to {/*{business_stage} */}their business of {/*{business_idea}*/}</p> 
+    <p> {formvalue.username}- is looking to {value}their business of business_idea</p> 
     <p>This enterprise has been operational since {formvalue.age_of_establishment}years and has been serving its customers since then</p>     
-<p>This establishment offers products/services like {formvalue.primary_products} to {/*{$offered_to}*/}</p>
-<p>Other products of the enterprise shall include-{/*{processed_}*/}</p>
+<p>This establishment offers products/services like {formvalue.primary_products} to {offered_to}</p>
 <p>In addition, the enterprise shall also be invloved in {formvalue.secondary_product_service_offered}</p>
 <p>other products of the enterprise shall include in{formvalue.processed_products}</p>
 <p>{formvalue.username} has relevant experience of {formvalue.relevant_experience} years in the field </p>
-<p>The entrepreneur {/*{skill_training}*/} in this field of work</p>
-<p>The enterprise is uniquely positioned because of its</p>
-<p>The {/*{establishment_type}*/} is located in {/*{business_locality}*/} area of{formvalue.business_area} in a {/*{infra_ownership}*/} property</p>
+<p>The entrepreneur {skill_training} in this field of work</p>
+<p>The enterprise is uniquely positioned because of its{select.map((e,i)=>{
+  return (
+<>
+<p key={i}>{e}</p>
+</>
+)})}</p>
+<p>The {business_locality} is located in  area of{formvalue.business_area} in a {infra_ownership} property</p>
 <p>The size of the establishment is{formvalue.establishment_area}sq ft</p>
-<p>This locality is selected because of {/*{reason_for_location}*/}</p>
-<p>{/*{market_research}*/} and the range of products and target market has been identified after that.<br/>The enterprise shall focus on offering its products/services to {/*{primary_market}*/}markets<br/>Our customers shall include -{formvalue.customers}<br/>The nature of the business is such that we expect {/*{$seasonality}*/}.Regarding competition, there are {/*{$competition}*/}.The enterprise shall procure goods/raw materials from {formvalue.suppliers}.Our marketing avenues to reach the targeted customers shall include- {/*{$marketing_avenues}*/}.The various foreseeable aveneus of scaling up and growing the business in the foreseeable future include {/*{scaleup_potential}*/}
+<p>This locality is selected because of {reasonLocation.map((e,i)=>{return(
+  <>
+    <p key={i}>{e}</p>
+  </>
+)})}</p>
+<p>{market_search} and the range of products and target market has been identified after that.<br/>The enterprise shall focus on offering its products/services to {primary_market}markets<br/>Our customers shall include -{formvalue.customers}<br/>The nature of the business is such that we expect {seasonality}.Regarding competition, there are {competition}.The enterprise shall procure goods/raw materials from {formvalue.suppliers}.Our marketing avenues to reach the targeted customers shall include- {market_search.map((e,i)=>{return(
+  <>
+    <p key={i}>{e}</p>
+  </>
+)})}.The various foreseeable aveneus of scaling up and growing the business in the foreseeable future include {scale_avenues.map((e,i)=>{return(
+  <>
+    <p key={i}>{e}</p>
+  </>
+)})}
 </p>
 
 
